e4e9be8562e6da96a7458ce53c1b4285ed314c94
GwangjoGong <dwc05101@kaist.ac.kr>
2022-11-16 15:16:48 +0900

    feat: mod

--- src/shared/shared.utils.js
+++ src/shared/shared.utils.js
@@ -12,7 +12,7 @@ export const uploadToS3 = async (file, userId, folderName) => {
   const objectName = `${folderName}/${userId}-${Date.now()}-${filename}`;
   const { Location } = await new AWS.S3()
     .upload({
-      Bucket: "instaclone-uploads",
+      Bucket: "damons-instaclone-uploads",
       Key: objectName,
       ACL: "public-read",
       Body: readStream,
